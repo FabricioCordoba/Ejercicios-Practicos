7f1006f9f3a80b80bfdeb52d502035246b540d46
Fabricio Cordoba <fabricio.cbe@gmail.com>
2023-03-07 10:25:46 -0300

    registro auto finalizado

--- registroAuto.ts
+++ registroAuto.ts
@@ -1,64 +1,69 @@
 import { Auto } from "./auto";
+
 const fs = require("fs");
+
 let listaVehiculos: any[] = [];
+
+const data = fs.readFileSync("./listadoAuto.json", "utf8");
+    const vehiculos = JSON.parse(data);
+    for (let i = 0; i < vehiculos.length; i++) {
+      listaVehiculos.push(vehiculos[i]);
+    }
 
 export class RegistroAuto {
   //consultar listado.
 
   consultar() {
-    //NO TOCAR MAS-- MUESTRA LISTADO COMPLETO DE VEHICULOS.
-
-    const data = fs.readFileSync("./listadoAuto.json", "utf8");
-    const vehiculos = JSON.parse(data);
-    for (let i = 0; i < vehiculos.length; i++) {
-      listaVehiculos.push(vehiculos[i]);
-    }
-    console.log(listaVehiculos);
+    console.log(listaVehiculos)
   }
 
-  darAlta(auto: Auto, listaVehiculos: Auto[]) {
+  darAlta(auto: Auto, array: Auto[]) {
     //NO TOCAR--AGREGA NUEVO AUTO AL LISTADO
-    if (listaVehiculos.push(auto)) {
-      console.log("Se incorporo vehiculo ", auto.modelo, " para la venta");
-      this.consultar()
+    if (array.push(auto)) {
+      console.log("Se incorporo vehiculo ", auto, " para la venta");
+      this.consultar(); //opcional
     } else {
-      console.log("el auto ", auto.modelo, " no se pudo incorporar");
+      console.log("el auto ", auto, " no se pudo incorporar");
     }
   }
 
-  darBaja(nombre:string, listaVehiculos: Auto[]): any{
-    let autoBaja = listaVehiculos.findIndex(auto => auto.modelo === nombre);
+  darBaja(nombre: string, array: Auto[]): any {
+    let autoBaja = array.findIndex((auto) => auto.modelo === nombre);
 
-    console.log(autoBaja);
-    
-    /*if(autoBaja>=0){
-        listaVehiculos.splice(autoBaja, 1);
-        console.log("El auto ", autoBaja, "se dio de baja");  
-        return listaVehiculos;     
-    }else {
-        console.log("El auto ", autoBaja, " no pudo darse de baja porque debe patentes" );
-        
+    if (autoBaja >= 0) {
+      array.splice(autoBaja, 1);
+      console.log("El vehiculo ", nombre, "se dio de baja");
+      console.log(array);
+
+      return array;
+    } else {
+      console.log(
+        "El vehiculo ",
+        nombre,
+        " no pudo darse de baja porque no se encontro"
+      );
     }
-    */
   }
 
-  consultarAuto(modelo: string, listaVehiculos: Auto[]){
-    let autoEncontrado = listaVehiculos.find(auto => auto.modelo === modelo);
-    console.log(autoEncontrado)
-    if(autoEncontrado){
-        console.log(modelo, ' Esta disponible ', autoEncontrado)
-        return autoEncontrado
-    } else{
-        console.log(modelo, ' No esta disponible');
-        
+  consultarAuto(modelo: string, array: Auto[]) {//FUNCIONA
+    let autoEncontrado = array.find((auto) => auto.modelo === modelo);
+      if (autoEncontrado) {
+      console.log(modelo, " Esta disponible ", autoEncontrado);
+      return autoEncontrado;
+    } else {
+      console.log(modelo, " No esta disponible");
     }
+  }
 }
-}
+
 let suran = new Auto("Suran", "Renault", 2016, "Rojo", "Sedan", 59004);
+let corolla= new Auto("Corolla", "Toyota", 2011, "Gris", "Sedan", 202456);
 let vendedor = new RegistroAuto();
 
 //vendedor.consultar() //CONSULTA LISTADO DE AUTOS
-vendedor.consultarAuto("Clio", listaVehiculos)
-//vendedor.darAlta(suran, listaVehiculos);
-//vendedor.consultar();
-//vendedor.darBaja("Clio", listaVehiculos);
+//vendedor.consultarAuto("Clio", listaVehiculos); //CONSULTA POR UN AUTO ESPECIFICO
+//vendedor.darAlta(corolla, listaVehiculos); // agrega un nuevo auto
+//vendedor.darAlta(suran, listaVehiculos); // agrega un nuevo auto
+//vendedor.darBaja("S10", listaVehiculos); //Elimina un vehiculo y devuelve el listado modificado
+//console.log(listaVehiculos);
+
